7fbe502fd092f5a562b2812bd56bb5e78b24d456
SyafikhAL010907 <syafikhal09@gmail.com>
2026-04-11 00:49:15 +0700

    CP:Main  Aman New

--- frontend/portalmahasiswaptik/vite.config.ts
+++ frontend/portalmahasiswaptik/vite.config.ts
@@ -54,13 +54,25 @@ export default defineConfig(({ mode }) => ({
             src: '/pwa-icon.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
           },
           {
             src: '/pwa-icon.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
